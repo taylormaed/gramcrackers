e91164d2350263f3507d323d515e976170897f13
mtaylorelliott <tayls65@yahoo.com>
2016-09-14 20:52:26 -0500

    add combo

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,49 +1,44 @@
 class Word < ActiveRecord::Base
+	def self.find_anagrams(string)
+		combinations = []
+		# Convert word to an array of letters
+		letters = string.split(//)
+		# Create an array to store anagrams
+		anagrams = []
+		# Loop through each letter in letters
+		letters.each do |letter|
+			# Select the remaining letters
+			remaining = letters.select{ |l| l != letter }
+			# Create a new word by combining the letter + remaining letters
+			# Add a new word to the anagrams array
+			anagrams << letter + remaining.join('')
+			# Create a new word by combining the letter + reverse of the remaining letters
+			# Add new word to anagrams array
+			anagrams << letter + reverse_letters(remaining).join('')
+		end
 
-def self.find_anagrams(string)
-combinations = []
+		# Return anagrams array
+			anagrams.each do |combo|
+				if Word.find_by_text(combo).present?
+					combinations << combo
+				end
+			end
 
-    # Convert word to an array of letters
-    letters = string.split(//)
+		combinations
+	end
 
-    # Create an array to store our anagrams
-    anagrams = []
 
-    # Loop through each letter in letters
-    letters.each do |letter|
-    
-    # Select the remaining letters
-    remaining = letters.select { |l| l != letter}
+		def self.reverse_letters(letters)
+			# create a new array of 2 items
+			length = letters.length
+			reversed_letters = Array.new(length)
 
-# Create a new word by combining the letter + the remaining letters
-# Add new word to anagrams array
-anagrams << letter + remaining.join('')
+			# loop through letters and keep index
+			letters.each_with_index do |letter, index|
+				reversed_letters[length - index - 1] = letter
+			end
 
-# Create a new word by combining the letter + the reverse of the remaining letters
-# Add a new word to anagrams array
-anagrams << letter + reverse_letters(remaining).join('')
-end
-
-#Return anagram array
-anagrams.each do |combo|
-    if Word.find_by_text(combo).present?
-        combinations << combo
-    end
-end
-
-combinations
-end
-
-def self.reverse_letters(letters)
-# Create a new array of 2 items
-length = letters.length
-reversed_letters = Array.new(length)
-
-# Loop through letters and keep index
-letters.each_with_index do |letter, index|
-    reversed_letters[length - index - 1] = letter
-end
-
-reversed_letters
-end
+			reversed_letters
+		end
+	end
 
