f18a735766ab945c30bc5b13d733cd8a34f49687
mtaylorelliott <tayls65@yahoo.com>
2016-09-16 21:52:32 -0500

    error message and constraints

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,5 +10,36 @@ end
 
 post '/' do
 	@word = params[:word]
-	redirect "/anagrams/#{@word}"
+	if valid_input?(@word)
+    	redirect "/anagrams/#{@word}"
+    else 
+    	@error = "Words must have between one and three characters and NOT have duplicate characters!"
+		erb :index
+	end
+end
+
+def three_letters?(input)
+	if input.length <= 3 and input.length >= 1
+		return true
+	else
+		return false
+	end
+end
+
+def distinct_letters?(input)
+	letter_array = input.chars
+	unique_letters = letter_array.uniq
+	if unique_letters.length < letter_array.length
+		false
+	else
+		true
+	end
+end
+
+def valid_input?(input)
+	if three_letters?(input) && distinct_letters?(input)
+		true
+	else
+		false
+	end
 end
