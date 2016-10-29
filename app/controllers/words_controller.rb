e9978a93e54c7f5a8e51edaa8b68999f0164ebaa
mtaylorelliott <tayls65@yahoo.com>
2016-10-29 00:27:00 -0500

    new and form

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,6 +3,16 @@ get '/words' do
 	@words = Word.all
 	erb :"/words/index"
 end
+
+get '/words/new' do
+    @word = Word.new
+    erb :"words/new"
+end
+
+post 'words' do
+    Word.create(text: params[:text])
+    redirect "/words/#{word.id}"
+end
 	
 get '/words/:id' do
 	@word = Word.find(params[:id])
