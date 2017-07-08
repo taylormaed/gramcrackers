dd5e64745734e10ca1d5323c6d724f1cbc6c889e
mtaylorelliott <tayls65@yahoo.com>
2017-07-08 14:28:02 -0400

    edit routes

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -11,8 +11,27 @@ end
 
 post '/words' do
 	@word = Word.create(text: params[:text])
-    redirect "/words/#{@word.id}"
+	if @word.valid?
+		@word.save
+		redirect "/words/#{@word.id}"
+	else
+		@errors = "If your word is not in the dictionary, please add it."
+	end
 end
+
+get '/words/:id/edit' do
+	@word = Word.find(params[:id])
+	erb :"/words/edit"
+end
+
+put '/words/:id' do
+	@word = Word.find(params[:id])
+	@word.text = params[:text]
+	if @word.valid?
+		@word.save
+		redirect "/words/#{@word.id}"
+	else 
+		@errors = "Please be sure you have entered a word."
 
 get '/words/:id' do
 	@word = Word.find(params[:id])
